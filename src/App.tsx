03cce1767864c7a268cdaa282e52608a466c57bb
unknown <riasat01@outlook.com>
2023-12-30 03:31:48 +0600

    typescript starting

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import PageHeader from './layout/PageHeader'
+
+function App() {
+
+  return (
+    <>
+      <PageHeader />
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/PageHeader.tsx
@@ -0,0 +1,18 @@
+
+
+const PageHeader = () => {
+    return (
+        <div className="flex gap-10 lg:gap-20 justify-between">
+            <div className="flex gap-4 items-center flex-shrink-0">
+                <button></button>
+                <a href="/">
+                    <h1 className="text-3xl font-semibold"><span className="font-bold text-blue-500">T</span>tube</h1>
+                </a>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default PageHeader;
